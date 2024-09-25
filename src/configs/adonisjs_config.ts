305fdbe378f626060fead8d88df2ec973607962f
Fernando IL <luffynando@gmail.com>
2024-09-25 11:32:07 -0600

    fix: :bug: apply resolution of override rule for adonis controllers

--- src/configs/adonisjs_config.ts
+++ src/configs/adonisjs_config.ts
@@ -81,8 +81,9 @@ export const adonisjsConfig: ConfigWithExtends[] = [
     },
   },
   {
-    files: ['**/app/controles/**/*.ts'],
+    files: ['**/app/controllers/**/*.ts'],
     rules: {
+      '@typescript-eslint/explicit-function-return-type': 'off',
       '@typescript-eslint/explicit-module-boundary-types': 'off',
     },
   },
