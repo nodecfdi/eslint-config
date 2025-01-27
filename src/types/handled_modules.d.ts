30705f7485ad712199187a7a96b8177363980ce5
Fernando Isidro L <luffynando@gmail.com>
2025-01-26 18:34:20 -0600

    chore: :arrow_up: update dependencies

--- src/types/handled_modules.d.ts
+++ src/types/handled_modules.d.ts
@@ -1,2 +1,14 @@
-declare module '@eslint-community/eslint-plugin-eslint-comments/configs';
-declare module 'eslint-plugin-promise';
+declare module '@eslint-community/eslint-plugin-eslint-comments/configs' {
+  import { type TSESLint } from '@typescript-eslint/utils';
+
+  const recommended: TSESLint.FlatConfig.Config;
+  export = { recommended };
+}
+
+declare module 'eslint-plugin-promise' {
+  import { type TSESLint } from '@typescript-eslint/utils';
+
+  export const configs: {
+    'flat/recommended': TSESLint.FlatConfig.Config;
+  };
+}
