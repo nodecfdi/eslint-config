bc4c4680ee1cc61065712aa757000bf318fd0355
Fernando Isidro L <luffynando@gmail.com>
2025-02-26 22:09:41 -0600

    refactor: :boom: remove regexp and security plugins, better usage using sonarjs

--- src/configs/regexp_config.ts
+++
@@ -1,4 +0,0 @@
-import * as regexpPlugin from 'eslint-plugin-regexp';
-import { type ConfigWithExtends } from 'typescript-eslint';
-
-export const regexpConfig: ConfigWithExtends[] = [regexpPlugin.configs['flat/recommended']];
